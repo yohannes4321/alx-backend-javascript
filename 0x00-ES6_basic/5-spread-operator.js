f415aa517b12894111ffeaf092056382f63657fd
yohannes4321 <alemuyohannes960@gmail.com>
2024-10-01 05:08:08 +0300

    0x00-ES6_basic

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,1 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-    return initialNumber + expansion1989 + expansion2019;}
-    
+export default function concatArrays(array1,array2, string) {return [...array1,...array2,...string]}
