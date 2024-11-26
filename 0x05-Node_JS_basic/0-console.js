02fb191fedb7cfbf7998f7e36b8a295d151ab91a
yohannes4321 <alemuyohannes960@gmail.com>
2024-11-26 09:46:53 +0300

    node js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,15 +1,10 @@
 /**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  */
-// Arrow function
-const displayMessage = (message) => console.log(message);
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-// Default module export.
 module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
